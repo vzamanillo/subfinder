bca5bc0df25a26fae6fc057072f8ce861a8f3146
Víctor Zamanillo <bellzebu@hotmail.com>
2020-09-22 14:06:07 +0200

    Fixed Archiveis

--- pkg/subscraping/sources/archiveis/archiveis.go
+++ pkg/subscraping/sources/archiveis/archiveis.go
@@ -11,6 +11,7 @@ import (
 )
 
 type agent struct {
+	Name    string
 	Results chan subscraping.Result
 	Session *subscraping.Session
 }
@@ -26,7 +27,7 @@ func (a *agent) enumerate(ctx context.Context, baseURL string) {
 
 	resp, err := a.Session.SimpleGet(ctx, baseURL)
 	if err != nil {
-		a.Results <- subscraping.Result{Source: sourcename, Type: subscraping.Error, Error: err}
+		a.Results <- subscraping.Result{Source: a.Name, Type: subscraping.Error, Error: err}
 		a.Session.DiscardHTTPResponse(resp)
 		return
 	}
@@ -34,7 +35,7 @@ func (a *agent) enumerate(ctx context.Context, baseURL string) {
 	// Get the response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		a.Results <- subscraping.Result{Source: sourcename, Type: subscraping.Error, Error: err}
+		a.Results <- subscraping.Result{Source: a.Name, Type: subscraping.Error, Error: err}
 		resp.Body.Close()
 		return
 	}
@@ -43,12 +44,12 @@ func (a *agent) enumerate(ctx context.Context, baseURL string) {
 
 	src := string(body)
 	for _, subdomain := range a.Session.Extractor.FindAllString(src, -1) {
-		a.Results <- subscraping.Result{Source: sourcename, Type: subscraping.Subdomain, Value: subdomain}
+		a.Results <- subscraping.Result{Source: a.Name, Type: subscraping.Subdomain, Value: subdomain}
 	}
 
 	match1 := reNext.FindStringSubmatch(src)
 	if len(match1) > 0 {
-		a.enumerate(ctx, match1[1], sourcename)
+		a.enumerate(ctx, match1[1])
 	}
 }
 
@@ -62,6 +63,7 @@ func (s *Source) Run(ctx context.Context, domain string, session *subscraping.Se
 	results := make(chan subscraping.Result)
 
 	a := agent{
+		Name:    s.Name,
 		Session: session,
 		Results: results,
 	}
